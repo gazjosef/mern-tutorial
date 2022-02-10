4fd6dc70642c85005d0fb2b0011d82db1e371df8
Gareth Hind <garethjhind@gmail.com>
2022-02-10 11:30:03 +1100

    Delete Goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -32,7 +32,18 @@ const setGoal = asyncHandler(async (req, res) => {
 // @access  Private
 
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
 });
 
 // @desc    Delete Goals
@@ -40,7 +51,16 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @access  Private
 
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
