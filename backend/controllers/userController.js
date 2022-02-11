e8b2fd736ea27bd621e073f92a918157c1f13fb3
Gareth Hind <garethjhind@gmail.com>
2022-02-11 12:23:06 +1100

    User Routes & Controller Functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,22 @@ const registerUser = (req, res) => {
   res.json({ message: "Register User" });
 };
 
+// @desc    Authenticate a User
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = (req, res) => {
+  res.json({ message: "Login User" });
+};
+
+// @desc    Get User Data
+// @route   GET /api/users/me
+// @access  Public
+const getMe = (req, res) => {
+  res.json({ message: "User Data Display" });
+};
+
 module.exports = {
   registerUser,
+  loginUser,
+  getMe,
 };
